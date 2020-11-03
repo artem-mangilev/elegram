70537b36e5cf17aa2f9086c7b8a613f2863bcf8e
Artem <artmnglv@gmail.com>
2020-11-03 18:15:31 +0500

    Add properties to input-field

--- src/app/shared/input-field/input-field.component.ts
+++ src/app/shared/input-field/input-field.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-input-field',
   templateUrl: './input-field.component.html',
-  styleUrls: ['./input-field.component.scss']
+  styleUrls: ['./input-field.component.scss'],
 })
 export class InputFieldComponent implements OnInit {
+  @Input() id = ''
+  @Input() label = ''
+  // TODO: add styles for success status
+  @Input() status: 'default' | 'error' | 'success' = 'default'
+  @Input() type: 'text' | 'tel' = 'text'
+  @Input() value = ''
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
