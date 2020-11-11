796b9a066e368160c35d6cca23161af9e8c5845e
Artem <artmnglv@gmail.com>
2020-11-11 08:02:34 +0500

    Make structure more flat

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { AuthComponent } from './auth.component'
-import { SharedModule } from 'app/shared/shared.module';
-import { PhoneStepComponent } from './phone-step/phone-step.component';
-import { CountryDropdownComponent } from './phone-step/country-dropdown/country-dropdown.component'
+import { SharedModule } from 'app/shared/shared.module'
+import { PhoneStepComponent } from './phone-step/phone-step.component'
+import { CountryDropdownComponent } from './country-dropdown/country-dropdown.component'
 
 @NgModule({
   declarations: [AuthComponent, PhoneStepComponent, CountryDropdownComponent],
