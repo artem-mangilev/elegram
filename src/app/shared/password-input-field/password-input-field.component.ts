036c17a35c060afb39c72d6fa799ecd9dc11e34a
Artem <artmnglv@gmail.com>
2020-11-17 11:42:55 +0500

    Make password input an Angular control

--- src/app/shared/password-input-field/password-input-field.component.ts
+++ src/app/shared/password-input-field/password-input-field.component.ts
@@ -2,18 +2,28 @@ import {
   ChangeDetectionStrategy,
   Component,
   EventEmitter,
+  forwardRef,
   Input,
   OnInit,
   Output,
 } from '@angular/core'
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms'
 
 @Component({
   selector: 'app-password-input-field',
   templateUrl: './password-input-field.component.html',
   styleUrls: ['./password-input-field.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: PasswordInputFieldComponent,
+      multi: true,
+    },
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PasswordInputFieldComponent implements OnInit {
+export class PasswordInputFieldComponent
+  implements OnInit, ControlValueAccessor {
   @Input() id = ''
   @Input() label = ''
   @Input() status = 'default'
@@ -42,7 +52,20 @@ export class PasswordInputFieldComponent implements OnInit {
     this.type = 'password'
   }
 
+  onChange(value: string): void {}
+
   onUpdate(value: string): void {
     this.passwordUpdate.emit(value)
+    this.onChange(value)
   }
+
+  writeValue(value: string): void {
+    this.value = value
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn
+  }
+
+  registerOnTouched(): void {}
 }
