8ab86be36018b69fd5f29a09b0651d31ccd68ac8
Artem <artmnglv@gmail.com>
2020-11-23 11:32:39 +0500

    Do not allow input when phone is valid

--- src/app/auth/phone-step/phone-mask.directive.ts
+++ src/app/auth/phone-step/phone-mask.directive.ts
@@ -1,5 +1,4 @@
-import { Directive, HostListener } from '@angular/core'
-import { NgControl } from '@angular/forms'
+import { Directive, ElementRef, HostListener } from '@angular/core'
 
 import { PhoneService } from '../phone-step/phone.service'
 
@@ -7,19 +6,29 @@ import { PhoneService } from '../phone-step/phone.service'
   selector: '[appPhoneMask]',
 })
 export class PhoneMaskDirective {
+  private prevValid: boolean
+  private prevValue: string
+
   constructor(
-    private ngControl: NgControl,
+    private inputRef: ElementRef,
     private phoneService: PhoneService
   ) {}
 
-  @HostListener('ngModelChange', ['$event']) onModelChange(
-    event: string
-  ): void {
-    this.onInputChange(event)
-  }
+  @HostListener('input', ['$event.target.value'])
+  onInput(value: string): void {
+    const raw = this.phoneService.raw(value)
+    const formatted = this.phoneService.format(raw)
 
-  onInputChange(value: string): void {
-    const formatted = this.phoneService.format(this.phoneService.raw(value))
-    this.ngControl.valueAccessor.writeValue(formatted)
+    if (
+      this.prevValid &&
+      raw.length > this.phoneService.raw(this.prevValue).length
+    ) {
+      this.inputRef.nativeElement.value = this.prevValue
+    } else {
+      this.inputRef.nativeElement.value = formatted
+
+      this.prevValid = this.phoneService.isValid(formatted)
+      this.prevValue = formatted
+    }
   }
 }
