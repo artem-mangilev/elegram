ef227c5282c3edbc10188f2a71938d4f15d4cfe9
Artem <artmnglv@gmail.com>
2020-11-24 13:47:18 +0500

    Pass phone to code step

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
 import { AuthComponent } from './auth.component'
+import { CodeStepComponent } from './code-step/code-step.component'
 import { PhoneStepComponent } from './phone-step/phone-step.component'
 
 const authRoutes: Routes = [
@@ -12,6 +13,10 @@ const authRoutes: Routes = [
       {
         path: 'phone',
         component: PhoneStepComponent,
+      },
+      {
+        path: 'code/:phone',
+        component: CodeStepComponent,
       },
     ],
   },
--- src/app/auth/code-step/code-step.component.ts
+++ src/app/auth/code-step/code-step.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-code-step',
   templateUrl: './code-step.component.html',
   styleUrls: ['./code-step.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CodeStepComponent implements OnInit {
-
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      console.log(params.get('phone'))
+    })
   }
-
 }
