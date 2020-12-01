0a6d1a429ad6ed2b8af7564d6aa2fe9f83bc68b8
Artem <artmnglv@gmail.com>
2020-12-01 16:28:12 +0500

    Create auth service

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core'
+
+import { MTProto } from '@mtproto/core'
+
+import { AppConfig } from '../../environments/environment'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private mtproto: MTProto
+  private phoneNumber: string
+  private phoneCodeHash: string
+
+  constructor() {
+    this.mtproto = new MTProto({
+      api_id: AppConfig.apiId,
+      api_hash: AppConfig.apiHash,
+      test: true,
+    })
+  }
+
+  setPhoneNumber(internationalPhoneNumber: string): Promise<unknown> {
+    return this.mtproto
+      .call('auth.sendCode', {
+        phone_number: internationalPhoneNumber,
+        settings: {
+          _: 'codeSettings',
+        },
+      })
+      .then((response) => {
+        console.log(response)
+
+        this.phoneNumber = response.phone_number
+        this.phoneCodeHash = response.phone_code_hash
+      })
+  }
+
+  signIn(phoneCode: string): Promise<unknown> {
+    return this.mtproto
+      .call('auth.signIn', {
+        phone_code: phoneCode,
+        phone_number: this.phoneNumber,
+        phone_code_hash: this.phoneCodeHash,
+      })
+      .then((response) => {
+        console.log(response)
+      })
+  }
+}
