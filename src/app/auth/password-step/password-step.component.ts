a194f449bdc867800e53b051715d669e43b9d8cb
Artem <artmnglv@gmail.com>
2020-12-03 14:23:53 +0500

    Add some markup and styles

--- src/app/auth/password-step/password-step.component.ts
+++ src/app/auth/password-step/password-step.component.ts
@@ -7,6 +7,8 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PasswordStepComponent implements OnInit {
+  inputLabel = 'Password'
+
   constructor() {}
 
   ngOnInit(): void {}
