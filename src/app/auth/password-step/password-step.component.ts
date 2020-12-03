ebd09ce9d99ecf0ccdb1c47912afb666a021eeb5
Artem <artmnglv@gmail.com>
2020-12-03 16:39:01 +0500

    Add comment with animation flow for future

--- src/app/auth/password-step/password-step.component.ts
+++ src/app/auth/password-step/password-step.component.ts
@@ -1,4 +1,13 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core'
+
+import lottie from 'lottie-web'
 
 @Component({
   selector: 'app-password-step',
@@ -6,10 +15,33 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
   styleUrls: ['./password-step.component.scss', './../step.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PasswordStepComponent implements OnInit {
+export class PasswordStepComponent implements OnInit, AfterViewInit {
   inputLabel = 'Password'
+
+  @ViewChild('monkeyContainer') monkeyContainer: ElementRef
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  // animation flow:
+  // show Idle
+  // when user starts typing,
+  // wait for Idle end, remove it
+  // if password hidden, run Close to the middle and stop
+  // if input is clear and password is hidden, run Close from the middle to the end and stop
+  // if password shown, run CloseAndPeek to the end
+  // if input is clear and password is shown, run CloseAndPeekToIdle
+  // if input is not clear and password changed from hidden to shown, run Peek to the middle
+  // if input is not clear and password changed from shown to hidden, run Peek from middle to the end
+
+  ngAfterViewInit(): void {
+    lottie.loadAnimation({
+      container: this.monkeyContainer.nativeElement,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: '../../../assets/monkey/TwoFactorSetupMonkeyIdle.json',
+    })
+  }
 }
